f04737a023e9c311c8ce3c62c2d0d7c5a6823a41
NikitaHeraimovych <ngerajmovic@gmail.com>
2021-06-25 01:06:17 +0200

    update

--- Task1/src/com/gmail/nikita00/wade/Main.java
+++ Task1/src/com/gmail/nikita00/wade/Main.java
@@ -3,6 +3,6 @@ package com.gmail.nikita00.wade;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println ("Hello World" );
     }
 }
