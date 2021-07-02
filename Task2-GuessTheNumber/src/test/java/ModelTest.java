30f8d04d748079131dce61dcea91e064b610c003
NikitaHeraimovych <ngerajmovic@gmail.com>
2021-07-02 17:04:48 +0200

    commit

--- Task2-GuessTheNumber/src/test/java/ModelTest.java
+++ Task2-GuessTheNumber/src/test/java/ModelTest.java
@@ -1,16 +1,23 @@
 import com.gmail.nikita00.wade.GlobalConstants;
 import com.gmail.nikita00.wade.Model;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
 public class ModelTest {
+    private static Model model;
+
+
+    @BeforeAll
+    public static void init (){
+        model = new Model ();
+        model.setPrimaryBarrier (GlobalConstants.PRIMARY_MIN_BARRIER, GlobalConstants.PRIMARY_MAX_BARRIER);
+    }
     @Test
     @DisplayName (value = "Random number is less then 100 and greater then 0")
     void randomNumberInDiapason () {
-        Model model = new Model ();
-        model.setPrimaryBarrier (GlobalConstants.PRIMARY_MIN_BARRIER, GlobalConstants.PRIMARY_MAX_BARRIER);
         for (int i = 0; i < 1000000000; i++) {
             model.setRandomNumber ();
             Assertions.assertTrue (model.getRandomNumber () < GlobalConstants.PRIMARY_MAX_BARRIER &&
