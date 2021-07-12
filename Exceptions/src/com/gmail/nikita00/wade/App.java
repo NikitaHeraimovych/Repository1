d638c7b1533be2c7253176d2d149b13aacc75a33
NikitaHeraimovych <ngerajmovic@gmail.com>
2021-07-12 12:12:36 +0200

    hw

---
+++ Exceptions/src/com/gmail/nikita00/wade/App.java
@@ -0,0 +1,75 @@
+package com.gmail.nikita00.wade;
+
+import java.io.EOFException;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class App {
+    // пугаем ОБОИМИ исключениями
+    public static void main(String[] args) throws EOFException, FileNotFoundException {
+        if (System.currentTimeMillis() % 2 == 0) {
+            throw new EOFException();
+        } else {
+            throw new FileNotFoundException();
+        }
+    }
+
+    // пугаем ПРЕДКОМ исключений
+    public static void main(String[] args) throws IOException {
+        if (System.currentTimeMillis() % 2 == 0) {
+            throw new EOFException();
+        } else {
+            throw new FileNotFoundException();
+        }
+    }
+
+    // EOFException перехватили catch-ом, им не пугаем
+    public static void main(String[] args) throws FileNotFoundException {
+        try {
+            if (System.currentTimeMillis() % 2 == 0) {
+                throw new EOFException();
+            } else {
+                throw new FileNotFoundException();
+            }
+        } catch (EOFException e) {
+            // ...
+        }
+    }
+
+    // ТЕПЕРЬ пугаем Throwable
+    public static void main(String[] args) throws Throwable {
+        try {
+            Throwable t = new Exception(); // а лететь будет Exception
+            throw t;
+        } catch (Exception e) { // и мы перехватим Exception
+            System.out.println("Перехвачено!");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.err.println("#1.in");
+        f(); // создаем фрейм, помещаем в стек, передаем в него управление
+        System.err.println("#1.out"); // ПРОПУСТИЛИ!
+    }
+
+    public static void f() {
+        System.err.println(".   #2.in");
+        g(); // создаем фрейм, помещаем в стек, передаем в него управление
+        System.err.println(".   #2.out"); // ПРОПУСТИЛИ!
+    }
+
+    public static void g() {
+        System.err.println(".   .   #3.in");
+        h(); // создаем фрейм, помещаем в стек, передаем в него управление
+        System.err.println(".   .   #3.out"); // ПРОПУСТИЛИ!
+    }
+
+    public static void h() {
+        System.err.println(".   .   .   #4.in");
+        if (true) {
+            System.err.println(".   .   .   #4.THROW");
+            throw new Error(); // выходим со всей пачки фреймов ("раскрутка стека") по 'throw'
+        }
+        System.err.println(".   .   .   #4.out"); // ПРОПУСТИЛИ!
+    }
+}
---
+++ Exceptions/src/com/gmail/nikita00/wade/Main.java
@@ -0,0 +1,8 @@
+package com.gmail.nikita00.wade;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
